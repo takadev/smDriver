59759207b9157e4ef0cfdf7d5fc008e33f5a91a7
t.kaneko <nontan20xx@gmail.com>
2018-07-02 14:25:07 +0900

    Initial commit

---
+++ smDriver/smDriver/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  smDriver
+//
+//  Created by Takahiro Kaneko on 2018/07/02.
+//  Copyright © 2018年 TK. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
